28939636f02c1c5a54d8cfb2cabe4da0f76b618b
Azizjon <azizjonsheronov2607@gmail.com>
2022-09-15 15:17:57 +0500

    Main flow

---
+++ app/src/main/java/com/example/myspotifyclone/fragment/main/home/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.myspotifyclone.fragment.main.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myspotifyclone.R
+
+
+class HomeFragment : Fragment() {
+
+}
